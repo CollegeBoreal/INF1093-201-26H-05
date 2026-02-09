eec2ab3ea0be80451edff6055bf20d99104bf5c1
B300098957 <300098957@collegeboreal.ca>
2026-02-09 11:28:24 -0500

    .

---
+++ 3.Tri/300098957/rapide/main.py
@@ -0,0 +1,15 @@
+with open("entree_quick.txt", "r") as f:
+    tab = list(map(int, f.read().split()))
+
+def tri_rapide(tab):
+    if len(tab) <= 1:
+        return tab
+    pivot = tab[len(tab) // 2]
+    gauche = [x for x in tab if x < pivot]
+    milieu = [x for x in tab if x == pivot]
+    droite = [x for x in tab if x > pivot]
+    return tri_rapide(gauche) + milieu + tri_rapide(droite)
+
+tab_trie = tri_rapide(tab)
+print("Résultat :", tab_trie)
+
