49f11bf61518668d5f7cb1b3152067f5db00f2ee
Rougaiyatoudiallo <118176543+Rougaiyatoudiallo@users.noreply.github.com>
2026-03-10 11:49:07 -0400

    TP Hashing

---
+++ 4.Hashing/300156627/ex4_dict.py
@@ -0,0 +1,10 @@
+notes = {
+    "Alice": 85,
+    "Bob": 78,
+    "Charlie": 92,
+    "Diana": 88
+}
+
+with open("resultats/ex4.txt", "w") as f:
+    for nom, note in notes.items():
+        f.write(f"{nom}:{note}\n")  
