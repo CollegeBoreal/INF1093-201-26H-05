cf5e3be61aade4e4643b00c458e965f69b02e9c9
lounesallouti <300151781@monboreal.ca>
2026-03-19 09:20:33 -0400

    Delete 4.Hashing/300151781/uppercase.py

--- 4.Hashing/300151781/uppercase.py
+++
@@ -1,7 +0,0 @@
-# uppercase.py - Convertit le contenu de entree.txt en majuscules dans sortie.txt
-
-with open("entree.txt", "r") as f_in, open("sortie.txt", "w") as f_out:
-    for ligne in f_in:
-        f_out.write(ligne.upper())
-
-print("Conversion en majuscules terminée. Résultat dans sortie.txt")
