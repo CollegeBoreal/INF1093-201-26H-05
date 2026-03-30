6394fcfa3c5a36b50605f742627121a530c18c61
abdelkader messali <kadermessalif@gmail.com>
2026-03-30 13:33:26 -0400

    Update exo1-hash.py

--- 4.Hashing/300157734/exo1-hash.py
+++ 4.Hashing/300157734/exo1-hash.py
@@ -1,1 +1,14 @@
+def hash_simple(mot, taille):
+    total = 0
+    for lettre in mot:
+        total += ord(lettre)
+    return total % taille
 
+
+mots = ["canard", "bateau", "lapin", "java"]
+taille_table = 10
+
+with open("resultats/ex1.txt", "w") as f:
+    for mot in mots:
+        valeur = hash_simple(mot, taille_table)
+        f.write(f"{mot}:{valeur}\n")
