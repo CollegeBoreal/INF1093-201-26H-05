72334ef7612358ded82944f88d5e9bef32e7a39b
setrar <setrar@valiha.com>
2026-03-30 10:15:59 -0400

    .

---
+++ 8.Dijkstra/.scripts/functions.ps1
@@ -0,0 +1,85 @@
+#!/usr/bin/env pwsh
+
+# ---------- Fonctions ----------
+function Test-Structure {
+    param(
+        [string]$Path
+    )
+
+    $paths = @(
+        "$Path/graph.py",
+        "$Path/dijkstra_tp.py",
+        "$Path/check_results.py"
+    )
+
+    foreach ($f in $paths) {
+        if (-not (Test-Path $f)) {
+            return ":x:"
+        }
+    }
+
+    return ":building_construction:"
+}
+
+function Get-StudentPaths {
+    param(
+        [string]$StudentID
+    )
+
+    return @{
+        README   = "$StudentID/README.md"
+        Images   = "$StudentID/images"
+        STRUCT   = "$StudentID/"
+    }
+}
+
+function Get-StudentChecks {
+    param(
+        [hashtable]$Paths
+    )
+
+    return @{
+        README    = Test-CommonItemExists -Path $Paths.README -IsReadme
+        Images    = Test-CommonItemExists -Path $Paths.Images
+        STRUCT    = Test-Structure        -Path $Paths.STRUCT
+    }
+}
+
+function Test-AllRequiredFilesPresent {
+    param(
+        [hashtable]$Checks
+    )
+
+    $validReadmeValues = @(
+        ":1st_place_medal:",
+        ":2nd_place_medal:"
+    )
+
+    return (
+        $Checks.README -in $validReadmeValues -and
+        $Checks.Images -eq ":heavy_check_mark:" -and
+        $Checks.STRUCT  -eq ":building_construction:"
+    )
+}
+
+function Write-PresenceHeader {
+    Write-Output ""
+    Write-Output "## :a: Présence"
+    Write-Output ""
+
+    Write-Output "|:hash:| Boréal :id: | README.md | images | Structure |"
+    Write-Output "|------|-------------|-----------|--------|-----------|"
+}
+
+
+function Write-StudentRow {
+    param(
+        [int]$Index,
+        [string]$StudentID,
+        [string]$GitHubLink,
+        [string]$ReadmePath,
+        [hashtable]$Checks
+    )
+
+    Write-Output "| $Index | [$StudentID](../$ReadmePath) :point_right: $GitHubLink | $($Checks.README) | $($Checks.Images) | $($Checks.STRUCT) |"
+}
