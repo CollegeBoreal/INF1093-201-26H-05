6379729d73f8dc173e96367169d0b6e46c158f45
samyzahir120-wq <samyzahir120@gmail.com>
2026-03-31 12:58:00 -0700

    Update ex3_search.py

--- 4.Hashing/300153710/ex3_search.py
+++ 4.Hashing/300153710/ex3_search.py
@@ -1,1 +1,40 @@
+def hash_simple(cle, taille):
+    total = 0
+    for lettre in cle:
+        total += ord(lettre)
+    return total % taille
 
+
+def inserer(table, cle, valeur):
+    index = hash_simple(cle, len(table))
+    table[index].append((cle, valeur))
+
+
+def rechercher(table, cle):
+    index = hash_simple(cle, len(table))
+    for k, v in table[index]:
+        if k == cle:
+            return v
+    return None
+
+
+table = [[] for _ in range(5)]
+
+donnees = [
+    ("Nasro", 15),
+    ("Djawed", 90),
+    ("Lahlou", 58),
+    ("Ahmed", 60),
+    ("Sara", 60)
+]
+
+for cle, valeur in donnees:
+    inserer(table, cle, valeur)
+
+
+cles_recherche = ["alice", "bob", "zoe"]
+
+with open("resultats/ex3.txt", "w") as f:
+    for cle in cles_recherche:
+        resultat = rechercher(table, cle)
+        f.write(f"{cle}:{resultat}\n")
