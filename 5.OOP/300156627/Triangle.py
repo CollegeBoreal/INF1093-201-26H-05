da475f62ede7bb403834b2d9c454665058ebf0fc
Rougaiyatou Diallo <rougaiyatoudiallo2000@gmail.com>
2026-03-30 22:21:51 -0400

    L'ajout de toutes mes modifications

---
+++ 5.OOP/300156627/Triangle.py
@@ -0,0 +1,20 @@
+"""
+Fichier : Triangle.py
+Description : Classe Triangle héritant de Figure
+Auteur : [Ton ID Boreal]
+Date : 2026-03-30
+"""
+
+from figure import Figure
+
+class Triangle(Figure):
+    def __init__(self, base, hauteur):
+        super().__init__("Triangle")
+        self.base = base
+        self.hauteur = hauteur
+
+    def aire(self):
+        return (self.base * self.hauteur) / 2
+
+    def afficher_info(self):
+        return f"{super().afficher_info()}, base={self.base}, hauteur={self.hauteur}, aire={self.aire()}"
