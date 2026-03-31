25409d1c4fcb3f41e1d6207928f9743591cb9fc7
samyzahir120-wq <samyzahir120@gmail.com>
2026-03-31 12:58:17 -0700

    Update ex4_dict.py

--- 4.Hashing/300153710/ex4_dict.py
+++ 4.Hashing/300153710/ex4_dict.py
@@ -1,1 +1,11 @@
+notes = {
+    "Nasro": 15,
+    "Djawed": 90,
+    "Lahlou": 58,
+    "Ahmed": 60,
+    "Sara": 100
+}
 
+with open("resultats/ex4.txt", "w") as f:
+    for nom, note in notes.items():
+        f.write(f"{nom}:{note}\n")
