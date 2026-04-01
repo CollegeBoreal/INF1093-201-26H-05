538008e0a8dd8349dc0b914a5ceda126c35c4548
Abdenour Mehani <300157301@monboreal.ca>
2026-04-01 18:44:54 -0400

    Update ex5_wordcount.py

--- 4.Hashing/300157301/ex5_wordcount.py
+++ 4.Hashing/300157301/ex5_wordcount.py
@@ -1,1 +1,17 @@
 
+texte = "python est simple et python est puissant"
+
+mots = texte.split()
+
+compteur = {}
+
+for mot in mots:
+    if mot in compteur:
+        compteur[mot] += 1
+    else:
+        compteur[mot] = 1
+
+
+with open("resultats/ex5.txt", "w") as f:
+    for mot, nb in compteur.items():
+        f.write(f"{mot}:{nb}\n")
