283000a2e56bf097da67afb1a926a038e01a7cca
Abdenour Mehani <300157301@monboreal.ca>
2026-04-01 18:40:22 -0400

    Update ex1_hash.py

--- 4.Hashing/300157301/ex1_hash.py
+++ 4.Hashing/300157301/ex1_hash.py
@@ -1,1 +1,15 @@
 
+def hash_simple(mot, taille):
+    total = 0
+    for lettre in mot:
+        total += ord(lettre)
+    return total % taille
+
+
+mots = ["chat", "chien", "oiseau", "python"]
+taille_table = 10
+
+with open("resultats/ex1.txt", "w") as f:
+    for mot in mots:
+        valeur = hash_simple(mot, taille_table)
+        f.write(f"{mot}:{valeur}\n")
