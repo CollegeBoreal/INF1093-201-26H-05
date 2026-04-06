63afc0cc989d7d4f0673a07d66ad1a46e577320b
maouchimedchafik-lgtm <maouchimedchafik@gmail.com>
2026-04-06 14:42:25 -0400

    Update parallelogramme.py

--- 5.OOP/300154142/parallelogramme.py
+++ 5.OOP/300154142/parallelogramme.py
@@ -1,8 +1,9 @@
 """
 Fichier : parallelogramme.py
 Description : Classe  parallelogramme héritant de Figure
-Auteur : [300155187]
-Date : 2005-11-25
+    | Nom | Maouchi Mohamed Chafik |
+    | 🆔  | 300154142          |
+
 """
 
 from figure import Figure
