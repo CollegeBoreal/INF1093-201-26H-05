9cbc8858a86ac2ce0bac792ac34e33958ad90c91
maouchimedchafik-lgtm <maouchimedchafik@gmail.com>
2026-04-06 14:41:53 -0400

    Update prisme_losange.py

--- 5.OOP/300154142/prisme_losange.py
+++ 5.OOP/300154142/prisme_losange.py
@@ -1,8 +1,9 @@
 """
 Fichier : prisme_losange.py
 Description : Classe prisme losange héritant de Figure
-Auteur : [300155187]
-Date : 2005-11-25
+    | Nom | Maouchi Mohamed Chafik |
+    | 🆔  | 300154142          |
+
 """
 
 from figure import Figure
