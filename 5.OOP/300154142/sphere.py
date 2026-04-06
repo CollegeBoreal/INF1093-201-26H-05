d2c829086f5723497fb0d1a9f50f96acf518c750
maouchimedchafik-lgtm <maouchimedchafik@gmail.com>
2026-04-06 14:22:14 -0400

    Update sphere.py

--- 5.OOP/300154142/sphere.py
+++ 5.OOP/300154142/sphere.py
@@ -1,8 +1,8 @@
 """
 Fichier : sphere.py
 Description : Classe sphere héritant de Figure
-Auteur : [300155187]
-Date : 2005-11-25
+Auteur : [300154142]
+Date : 2026-04-06
 """
 
 from figure import Figure
