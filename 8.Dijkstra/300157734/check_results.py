2cbe9da69adbb8caef8f9b3da0688a53be0669d8
abdelkader messali <kadermessalif@gmail.com>
2026-04-06 13:41:52 -0400

    Update check_results.py

--- 8.Dijkstra/300157734/check_results.py
+++ 8.Dijkstra/300157734/check_results.py
@@ -1,1 +1,13 @@
+from dijkstra_tp import shortest, g
 
+target = g.get_vertex('e')
+path = shortest(target)
+
+expected_path = ['a', 'c', 'f', 'e']
+
+if path == expected_path:
+    print("✅ Bravo, le chemin est correct !")
+else:
+    print("❌ Chemin incorrect.")
+    print("Votre chemin:", path)
+    print("Chemin attendu:", expected_path)
