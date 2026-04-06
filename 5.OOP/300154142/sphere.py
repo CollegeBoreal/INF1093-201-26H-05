77f96694079d2f2adc4d0a76b51934070e187fe0
maouchimedchafik-lgtm <maouchimedchafik@gmail.com>
2026-04-06 14:41:20 -0400

    Update sphere.py

--- 5.OOP/300154142/sphere.py
+++ 5.OOP/300154142/sphere.py
@@ -1,8 +1,9 @@
 """
 Fichier : sphere.py
 Description : Classe sphere héritant de Figure
-Auteur : [300155187]
-Date : 2005-11-25
+    | Nom | Maouchi Mohamed Chafik |
+    | 🆔  | 300154142          |
+
 """
 
 from figure import Figure
