d4d7b87390a3fec99ee809ded49df03db2d582c6
toufik <300157330@monboreal.ca>
2026-04-06 15:24:46 -0400

    Ajout des exercices de tri insertion shell quick

---
+++ 3.Tris/300157330/shell/main.py
@@ -0,0 +1,22 @@
+with open("entree_shell.txt", "r", encoding="utf-8") as f:
+    tab = list(map(int, f.read().split()))
+
+def tri_shell(tab):
+    n = len(tab)
+    ecart = n // 2
+
+    while ecart > 0:
+        for i in range(ecart, n):
+            temp = tab[i]
+            j = i
+
+            while j >= ecart and tab[j - ecart] > temp:
+                tab[j] = tab[j - ecart]
+                j -= ecart
+
+            tab[j] = temp
+
+        ecart //= 2
+
+tri_shell(tab)
+print("Résultat :", tab)
