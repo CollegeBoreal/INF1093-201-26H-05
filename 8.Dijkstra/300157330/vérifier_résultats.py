f199a3b504ee099ae141b9fcc7938a7577bb2081
toufikmekbel <toufikmekbel8@gmail.com>
2026-04-06 21:09:03 -0400

    Delete 8.Dijkstra/300157330 directory

--- 8.Dijkstra/300157330/vérifier_résultats.py
+++
@@ -1,9 +0,0 @@
-from dijkstra_tp import g, reconstruire_chemin
-
-arrivee = g.obtenir_sommet("e")
-chemin = reconstruire_chemin(arrivee)
-
-if chemin == ['a', 'c', 'f', 'e']:
-    print("OK")
-else:
-    print("ERREUR")
