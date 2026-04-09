82e088dd8a4edd56cc89d8057e7dbe83ea15993b
benhammoucheliza928-eng <benhammoucheliza928@gmail.com>
2026-04-09 10:51:54 -0400

    Update main.py

--- 3.Tris/300151835/insertion/main.py
+++ 3.Tris/300151835/insertion/main.py
@@ -1,1 +1,19 @@
 
+def tri_insertion(tab):
+    for i in range(1, len(tab)):
+        cle = tab[i]
+        j = i - 1
+
+        while j >= 0 and tab[j] > cle:
+            tab[j + 1] = tab[j]
+            j -= 1
+
+        tab[j + 1] = cle
+
+
+with open("entree_insertion.txt", "r") as f:
+    tab = list(map(int, f.read().split()))
+
+tri_insertion(tab)
+
+print("Résultat :", tab)
