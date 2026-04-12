24d0d6638a68283f9ab3c83983744b5ffefa843e
Abdenour Mehani <300157301@monboreal.ca>
2026-04-12 02:32:19 -0400

    Update Prisme.py

--- 5.OOP/300157301/Prisme.py
+++ 5.OOP/300157301/Prisme.py
@@ -1,1 +1,11 @@
 
+class Prisme:
+    def __init__(self, aire_base, hauteur):
+        self.aire_base = aire_base
+        self.hauteur = hauteur
+
+    def volume(self):
+        return self.aire_base * self.hauteur
+
+    def afficher_info(self):
+        return f"Prisme d'aire de base {self.aire_base} et hauteur {self.hauteur}"
