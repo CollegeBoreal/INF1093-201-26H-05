ed8b54dd966081f8f506673457a589ec7d754e00
Abdenour Mehani <300157301@monboreal.ca>
2026-04-12 02:25:42 -0400

    Update Cube.py

--- 5.OOP/300157301/Cube.py
+++ 5.OOP/300157301/Cube.py
@@ -1,1 +1,13 @@
 
+class Cube:
+    def __init__(self, cote):
+        self.cote = cote
+
+    def aire(self):
+        return 6 * self.cote ** 2
+
+    def volume(self):
+        return self.cote ** 3
+
+    def afficher_info(self):
+        return f"Cube de côté {self.cote}"
