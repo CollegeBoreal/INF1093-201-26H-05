5724c18a7be21691c4e342d0ca465b5369bb567c
Abdenour Mehani <300157301@monboreal.ca>
2026-04-12 02:30:58 -0400

    Update Ovale.py

--- 5.OOP/300157301/Ovale.py
+++ 5.OOP/300157301/Ovale.py
@@ -1,1 +1,14 @@
 
+import math
+
+
+class Ovale:
+    def __init__(self, demi_grand_axe, demi_petit_axe):
+        self.demi_grand_axe = demi_grand_axe
+        self.demi_petit_axe = demi_petit_axe
+
+    def aire(self):
+        return math.pi * self.demi_grand_axe * self.demi_petit_axe
+
+    def afficher_info(self):
+        return f"Ovale de demi-axes {self.demi_grand_axe} et {self.demi_petit_axe}"
