d5463cb5ed572c1830f180e003b1ee2235ee5057
Mohammed-mati <mohmati1998@gmail.com>
2026-04-13 14:33:25 -0400

    Add files via upload

---
+++ 5.OOP/300158185/Triangle.py
@@ -0,0 +1,10 @@
+class Triangle:
+    def __init__(self, base, hauteur):
+        self.base = base
+        self.hauteur = hauteur
+
+    def aire(self):
+        return (self.base * self.hauteur) / 2
+
+    def afficher_info(self):
+        return f"Triangle de base {self.base} et hauteur {self.hauteur}"
