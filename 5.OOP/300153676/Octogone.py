9016fe34278589ad8aa8eb9d8fd338c6a5a47002
lyes medjahed <lyes.medjahed@monboreal.ca>
2026-04-13 13:56:42 -0400

    Update Octogone.py

--- 5.OOP/300153676/Octogone.py
+++ 5.OOP/300153676/Octogone.py
@@ -1,1 +1,11 @@
+class Octogone:
+    def __init__(self, cote, apothem):
+        self.cote = cote
+        self.apothem = apothem
 
+    def aire(self):
+        perimetre = 8 * self.cote
+        return (perimetre * self.apothem) / 2
+
+    def afficher_info(self):
+        return f"Octogone de côté {self.cote} et apothème {self.apothem}"
