ce70615a833b38a1655f4eb756eaea4347b01c73
Mohammed-mati <mohmati1998@gmail.com>
2026-04-13 14:27:02 -0400

    Delete 5.OOP/300158185 directory

--- 5.OOP/300158185/Figure.py
+++
@@ -1,8 +0,0 @@
-import math
-
-class Figure:
-    def aire(self):
-        raise NotImplementedError("La méthode aire() doit être redéfinie.")
-
-    def afficher_info(self):
-        return f"Figure : {self.__class__.__name__}"
