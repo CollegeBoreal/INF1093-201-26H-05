752f2a370e244a8814ec0858cf8c45ad21acca2d
aymen.ab <aymenbouchemal.ab@gmail.com>
2026-04-13 14:30:57 -0400

    Update sphere.py

--- 5.OOP/300155934/sphere.py
+++ 5.OOP/300155934/sphere.py
@@ -1,8 +1,8 @@
 """
 Fichier : sphere.py
 Description : Classe sphere héritant de Figure
-Auteur : [300151588]
-Date : 2005-11-25
+Auteur : [300155934]
+Date : 2005-05-20
 """
 
 from figure import Figure
