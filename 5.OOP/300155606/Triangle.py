0ff820eac39f49fa47ccd3c2168d4a3352b0a117
Djelloulmsili <djeloulmsili09@gmail.com>
2026-04-13 13:51:59 -0400

    Add Triangle class inheriting from Figure

---
+++ 5.OOP/300155606/Triangle.py
@@ -0,0 +1,24 @@
+"""
+Fichier : Triangle.py
+Description : Classe Triangle héritant de Figure
+Auteur : 300155606
+Date : 2025-04-13
+"""
+
+from figure import Figure
+
+class Triangle(Figure):
+    def __init__(self, base, hauteur):
+        super().__init__("Triangle")
+        self.base = base
+        self.hauteur = hauteur
+
+    def aire(self):
+        # Aire d'un triangl : (base * hauteur) / 2
+        return (self.base * self.hauteur) / 2
+
+    def afficher_info(self):
+        return (
+            f"{super().afficher_info()}, "
+            f"base={self.base}, hauteur={self.hauteur}, aire={self.aire()}"
+        )
