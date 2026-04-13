5ed223d540c7fdcc7e8d16d7e8717f4077b96b06
lyes medjahed <lyes.medjahed@monboreal.ca>
2026-04-13 13:55:38 -0400

    Update Losange.py

--- 5.OOP/300153676/Losange.py
+++ 5.OOP/300153676/Losange.py
@@ -1,1 +1,10 @@
+class Losange:
+    def __init__(self, diagonale1, diagonale2):
+        self.diagonale1 = diagonale1
+        self.diagonale2 = diagonale2
 
+    def aire(self):
+        return (self.diagonale1 * self.diagonale2) / 2
+
+    def afficher_info(self):
+        return f"Losange de diagonales {self.diagonale1} et {self.diagonale2}"
