895db5c4c8a889160f96ec6784d80c434066d717
lyes medjahed <lyes.medjahed@monboreal.ca>
2026-04-13 13:32:26 -0400

    Update ex2_insert.py

--- 4.Hashing/300153676/ex2_insert.py
+++ 4.Hashing/300153676/ex2_insert.py
@@ -1,1 +1,29 @@
+def hash_simple(cle, taille):
+    total = 0
+    for lettre in cle:
+        total += ord(lettre)
+    return total % taille
 
+
+def inserer(table, cle, valeur):
+    index = hash_simple(cle, len(table))
+    table[index].append((cle, valeur))
+
+
+table = [[] for _ in range(5)]
+
+donnees = [
+    ("Lyes", 15),
+    ("Melissa", 90),
+    ("Rabah", 58),
+    ("Ibrahim", 60),
+    ("Iba", 100)
+]
+
+for cle, valeur in donnees:
+    inserer(table, cle, valeur)
+
+
+with open("resultats/ex2.txt", "w") as f:
+    for i, case in enumerate(table):
+        f.write(f"{i}:{case}\n")
