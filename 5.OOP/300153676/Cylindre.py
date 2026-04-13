f2e0eceb5f58c9975e9256d42360221e8ee6d20f
lyes medjahed <lyes.medjahed@monboreal.ca>
2026-04-13 13:53:25 -0400

    Update Cylindre.py

--- 5.OOP/300153676/Cylindre.py
+++ 5.OOP/300153676/Cylindre.py
@@ -1,1 +1,16 @@
+import math
 
+
+class Cylindre:
+    def __init__(self, rayon, hauteur):
+        self.rayon = rayon
+        self.hauteur = hauteur
+
+    def aire(self):
+        return 2 * math.pi * self.rayon * (self.rayon + self.hauteur)
+
+    def volume(self):
+        return math.pi * self.rayon ** 2 * self.hauteur
+
+    def afficher_info(self):
+        return f"Cylindre de rayon {self.rayon} et hauteur {self.hauteur}"
