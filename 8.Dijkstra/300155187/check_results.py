30084c39b0f6bed9824f42cbb24f132f472a78ee
Maimouna <diallo1960maimouna@gmail.com>
2026-04-16 14:27:07 -0400

    Ajout de l heritage

--- 8.Dijkstra/300155187/check_results.py
+++ 8.Dijkstra/300155187/check_results.py
@@ -1,6 +1,7 @@
-# check_results.py
+from world_graph import WorldGraph
+from dijkstra_tp import dijkstra, shortest, path_distance
 
-from dijkstra_tp import g, dijkstra, shortest, path_distance
+g = WorldGraph()
 
 start_city = 'Toronto'
 target_city = 'Johannesburg'
@@ -8,26 +9,21 @@ target_city = 'Johannesburg'
 start = g.get_vertex(start_city)
 target = g.get_vertex(target_city)
 
-# 🔒 sécurité
-if not start or not target:
-    print("❌ Ville invalide")
-    exit()
-
 dijkstra(g, start)
 
 path = shortest(target)
 distance = path_distance(g, path)
 
-expected_path = [
+print("Chemin trouvé :", " → ".join(path))
+print("Distance :", distance, "km")
+
+expected = [
     'Toronto','London','Paris',
     'Casablanca','Dakar','Lagos','Nairobi','Johannesburg'
 ]
 
-print("Chemin trouvé :", " → ".join(path))
-print("Distance :", distance, "km")
-
-if path == expected_path:
-    print("\n✅ Bravo, le chemin est correct !")
+if path == expected:
+    print("✅ Correct")
 else:
-    print("\n❌ Chemin incorrect.")
-    print("Chemin attendu :", " → ".join(expected_path))
+    print("❌ Incorrect")
+    print("Attendu :", " → ".join(expected))
