ac84d5e61f56b267a54c062e91b09d8767412ba4
chouaibait <chouaitchou25@gmail.com>
2026-04-22 23:50:11 -0400

    Refactor quicksort implementation in main.py

--- 3.Tris/300155045/quick/main.py
+++ 3.Tris/300155045/quick/main.py
@@ -2,26 +2,17 @@ with open("entree_quick.txt", "r") as f:
     tab = list(map(int, f.read().split()))
 
 def tri_rapide(tab):
-    if len(tab) <= 1:  # SI longueur(tab) ≤ 1
-        return tab     # RETOURNER tab
-    
-    pivot = tab[len(tab) // 2]  # pivot ← tab[longueur(tab) ÷ 2]
-    
-    gauche = []    # liste vide
-    milieu = []
-    droite = []
-    
-    for x in tab:  # POUR chaque élément x DANS tab
-        if x < pivot:      # SI x < pivot
+    if len(tab) <= 1:
+        return tab
+    pivot = tab[len(tab) // 2]
+    gauche, milieu, droite = [], [], []
+    for x in tab:
+        if x < pivot:
             gauche.append(x)
-        elif x == pivot:   # SINON SI x = pivot
+        elif x == pivot:
             milieu.append(x)
-        else:              # SINON
+        else:
             droite.append(x)
-    
-    #RETOURNER concaténer(tri_rapide(gauche), milieu, tri_rapide(droite))
     return tri_rapide(gauche) + milieu + tri_rapide(droite)
 
-print("Avant:", tab)
-tab_trie = tri_rapide(tab)
-print("Après:", tab_trie)
+print("Résultat Quick :", tri_rapide(tab))
